2c8c33f19a9bfd85a56f73896e6418d10c29856f
Franciel <frannciel@gmail.com>
2017-10-15 16:56:43 -0300

    Update conexao.php

--- bd/conexao.php
+++ bd/conexao.php
@@ -9,12 +9,17 @@ class Conexao {
     }
 
     public static function getInstance() {
-        if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_a2e65a5cd7ae39b', 'b9fb1bd306346b', '12f65fd4', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        try{
+            if (!isset(self::$instance)) {
+                self::$instance = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_a2e65a5cd7ae39b', 'b9fb1bd306346b', '12f65fd4', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+            }
+            return self::$instance;
+        } catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
         }
-        return self::$instance;
+
     }
 }
 ?>
