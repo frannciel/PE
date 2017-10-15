7a71b5fa470258a8f40ff3c454f584dab82ef3d5
Franciel <frannciel@gmail.com>
2017-10-15 16:20:43 -0300

    Update conexao.php
    Banco do heroku

--- bd/conexao.php
+++ bd/conexao.php
@@ -10,7 +10,7 @@ class Conexao {
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=protocolo', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_a2e65a5cd7ae39b', 'b9fb1bd306346b', '12f65fd4', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
