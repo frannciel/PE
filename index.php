e4e4febe223d7c5ca2d0710569218bc5237a4607
Franciel <frannciel@gmail.com>
2017-10-16 00:17:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 ﻿<?php
 
-require_once 'php/controller.php';
+$PDO = Conexao::getInstance();
+$sql = $PDO->query("SELECT * FROM usuario WHERE  'email' = frannciel@gmail.com'");
+var_dump($sql->fetch(PDO::FETCH_OBJ));
 
-$usuario = Controller::getUsuario(array('email', 'frannciel@gmail.com'));
+class Conexao {
 
-var_dump($usuario)
+    public static $instance;
+
+    private function __construct() {
+        //
+    }
+
+    public static function getInstance() {
+        try{
+            if (!isset(self::$instance)) {
+                self::$instance = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_a2e65a5cd7ae39b', 'b9fb1bd306346b', '12f65fd4', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+            }
+            return self::$instance;
+            echo "Conetado ";
+        } catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+
+    }
+}
 ?>
