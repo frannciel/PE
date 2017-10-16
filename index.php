984e01cb79fb87f113289c9c6cd90f0d4799f8a5
Franciel <frannciel@gmail.com>
2017-10-16 00:39:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,9 @@ class Conexao {
                 self::$instance = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_a2e65a5cd7ae39b', 'b9fb1bd306346b', '12f65fd4', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+                echo "Conectando ";
             }
-            echo "Conetado ";
+            echo "Conectado ";
             return self::$instance;
 
         } catch(PDOException $e) {
