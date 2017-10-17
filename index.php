324df536d8cb13678af3333bc024a95ad93388f1
Franciel <frannciel@gmail.com>
2017-10-17 20:43:02 -0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ $db = substr($url["path"], 1);
 
 $conn = new PDO('mysql:host=' .$server.';dbname='.$db, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$conn->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING)   
+$conn->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);  
 echo 'Conectado </br>';
-
+var_dump($conn);
 $sql = $conn->query("SELECT * FROM usuario");
+var_dump($sql);
 $dados = $sql->fetch(PDO::FETCH_OBJ);
 print_r($dados);
 
