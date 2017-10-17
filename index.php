911f2a3b6b58b2f7ecbd58baadd11e9911edb107
Franciel <frannciel@gmail.com>
2017-10-17 11:50:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ $conn = new mysqli($server, $username, $password, $db);
 
 $sql = "SELECT * FROM `usuario`";
 $query = $conn->query($sql);
-var_dump($dados = $query->mysqli_fetch_array());
+var_dump($query->mysqli_fetch_array());
 
 ?>
