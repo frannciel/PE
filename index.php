1f182415292408b95541da2096fe0af782002d98
Franciel <frannciel@gmail.com>
2017-10-17 20:21:56 -0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $db = substr($url["path"], 1);
 
 //$conn = new mysqli($server, $username, $password, $db);
 
-$conn = new PDO("mysql:host=".$server.";dbname=".$db.", $username, $password); 
+$conn = new PDO("mysql:host=".$server.";dbname=".$db, $username, $password); 
 
 
 var_dump($conn);
