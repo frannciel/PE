b613299cc5686db318cd354a5260b34394367a0a
Franciel <frannciel@gmail.com>
2017-10-17 21:13:35 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+require_once 'bd/conexao.php';
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$conn = Conexao::getInstance();
 
-$conn = new PDO('mysql:host=' .$server.';dbname='.$db, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$conn->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);  
-echo 'Conectado </br>';
 var_dump($conn);
 $sql = $conn->query("SELECT * FROM usuario");
-var_dump($sql);
 $dados = $sql->fetch(PDO::FETCH_OBJ);
 print_r($dados);
 
-//$sql = "SELECT * FROM `usuario`";
 ?>
