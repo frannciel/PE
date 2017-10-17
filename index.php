fd28ee847bac414d92fd2d9265b981cfd2b471dd
Franciel <frannciel@gmail.com>
2017-10-17 11:30:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 ﻿<?php
-
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
-
-
+$link = mysql_connect($server, $db, $password);
+if (!$link) {
+    die('Erro ao conectar ao banco: ' . mysql_error());
+}
+echo 'Conectado com sucesso';
+mysql_close($link);
+echo $db . " - name<br>";
 $conn = new mysqli_connect($server, $username, $password, $db);
-
 if (!$conn) {
     echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
@@ -20,8 +21,6 @@ if (!$conn) {
 $sql = "SELECT * FROM usuario";
 $query = $conn->query($sql);
 echo 'Registros encontrados: ' . $query->num_rows;
-
-
 echo $db . " - name<br>";
 echo $server . " - host<br>";
 echo $username . " - user<br>";
