455c45ce498432489af619eb976c8ea9fd2663aa
Franciel <frannciel@gmail.com>
2017-10-17 12:01:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,19 @@ $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 
+//check connection 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
 $sql = "SELECT * FROM `usuario`";
 $query = $conn->query($sql);
-$dados = $query->mysqli_fetch_array();
+echo 'Registros encontrados: ' . $query->num_rows;
+$dados = $query->fetch_array();
 print_r($dados);
 
 echo 'Registros encontrados: ' . $query->num_rows;
+
+$conn->close();
 ?>
