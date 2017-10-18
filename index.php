fefcc36a82935d83a4c82b0500340eda7c5c48e5
Franciel <frannciel@gmail.com>
2017-10-18 00:45:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,15 @@ include 'php/controller.php';
 $usuario = Controller::geTest();
 echo ($usuario);
 
+//require_once 'bd/conexao.php';
+//$conn = Conexao::getInstance();
+//$sql = $conn->query("SELECT * FROM usuario");
+//$dados = $sql->fetch(PDO::FETCH_OBJ);
+//print_r($dados);
+
+
+$usuario = Controller::getUsuario(array(id, '1'));
+echo " passou 06";
+print_r($usuario);
+
 ?>
