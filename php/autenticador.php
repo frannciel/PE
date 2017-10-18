67dd3ff914ebd1a10df4375e5f4c50d5fcffd84e
Franciel <frannciel@gmail.com>
2017-10-18 01:14:33 -0300

    Update autenticador.php

--- php/autenticador.php
+++ php/autenticador.php
@@ -1,21 +1,19 @@
 <?php
-echo "passou 1";
 session_start(); // Inicia a sessão
-echo "passou 2";
 include 'controller.php';
-echo "passou 3";
 
 $email = isset($_POST["email"]) ? $_POST["email"] : '';
 $senha = isset($_POST["senha"]) ? $_POST["senha"] : '';
-echo "passou 4";
 $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-echo "passou 5";
 // Busca o usuário no banco de dados usando como parametro o email coletado na tela de login
 if($email != 1){
+	echo "passou 4";
 	$usuario = Controller::getUsuario(array('email', str_replace('\'','',str_replace('"','',$email ))));
-
+	echo "passou 6";
 	if(!empty($senha)){
+		echo "passou 7";
 		if(password_verify($senha, $usuario->senha)){
+			echo "passou 8";
 			$_SESSION['email']	= $email ;
 			$_SESSION['senha']	= $senha ;
 			$_SESSION['id'] = $usuario->id;
@@ -31,6 +29,7 @@ if($email != 1){
 		header("location:../index.php");
 	}
 }else{
+	echo "passou 9";
 	$_SESSION['error']	= true;
 	$_SESSION['email']	= $email ;
 	header("location:../index.php");
