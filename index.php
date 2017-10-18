f8afa85131de96e291be2be189eea5a2ff2d8cca
Franciel <frannciel@gmail.com>
2017-10-18 00:22:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'bd/conexao.php';
-require_once 'php/controller.php';
+
 
 $conn = Conexao::getInstance();
 $sql = $conn->query("SELECT * FROM usuario");
 $dados = $sql->fetch(PDO::FETCH_OBJ);
 print_r($dados);
 
+require_once 'php/controller.php';
 $usuario = Controller::getUsuario(array(id, '1'));
 print_r($usuario);
 
