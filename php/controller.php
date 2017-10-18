a9b905dc5896c39bbf197688ab55b82a49e71d8b
Franciel <frannciel@gmail.com>
2017-10-18 01:29:28 -0300

    Update controller.php

--- php/controller.php
+++ php/controller.php
@@ -136,9 +136,11 @@ class Controller {
     *@Return Um objeto do tipo usuário
     */
     public static function getUsuario($request) {
-        
+        echo "passou 10";
         $PDO = Conexao::getInstance();
+        echo "passou 11";
         $sql = $PDO->query("SELECT * FROM usuario WHERE ".$request[0]." = '".$request[1]."'");
+        echo "passou 12";
         return $sql->fetch(PDO::FETCH_OBJ);
     }
      /*
